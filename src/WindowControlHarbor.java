fc0be94fbbda7cd316fb700852ecd0d18adf41e9
Svetlnkk <89974865+Svetlnkk@users.noreply.github.com>
2021-11-11 16:05:43 +0400

    Lab4Java

--- src/WindowControlHarbor.java
+++ src/WindowControlHarbor.java
@@ -1,15 +1,26 @@
 import javax.swing.*;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
 import javax.swing.text.NumberFormatter;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.text.NumberFormat;
+import java.util.LinkedList;
 import java.util.Objects;
 
 public class WindowControlHarbor extends JPanel{
-    private Harbor<ITransport, LightMotor> harbor;
+    private LinkedList<ITransport> linkedListHarborRemoveBoat=new LinkedList<ITransport>();
+    private HarborCollection harborCollection;
+    private DefaultListModel<String> defListModelHarbors = new DefaultListModel<String>();
+    JList<String> jListHarbors;
+    JLabel labelHarbors = new JLabel("Гавани");
+    JTextField txtHarborName = new JTextField();
+    JButton btnAddHarbor = new JButton("Добавить гавань");
+    JButton btnRemoveHarbor = new JButton("Удалить гавань");
     JButton btnParkingSkiff=new JButton("<html>Припарковать<br>лодку");
     JButton btnParkingBoat=new JButton("<html>Припарковать<br>катер");
+    JButton btnShowBoat=new JButton("Показать");
     JButton btnTake=new JButton("Забрать");
     JLabel labelTake=new JLabel("Забрать водный транспорт");
     JLabel labelPlace=new JLabel("Место: ");
@@ -18,7 +29,9 @@ public class WindowControlHarbor extends JPanel{
     CanvasBoat canvasBoat=new CanvasBoat();
 
     private void Draw(Graphics g){
-        harbor.Draw(g);
+        if(jListHarbors.getSelectedValue()!=null){
+            harborCollection.getHarbor(jListHarbors.getSelectedValue()).Draw(g);
+        }
     }
     @Override
     public void paintComponent(Graphics g){
@@ -30,22 +43,53 @@ public class WindowControlHarbor extends JPanel{
         btn.setBounds(btnX, btnY, btnWidth, btnHeigth);
         add(btn);
     }
+    public class listBoxChangeListener implements ListSelectionListener {
+        @Override
+        public void valueChanged(ListSelectionEvent e) {
+            repaint();
+        }
+    }
+    public void ReloadHarbors(){
+        int index = jListHarbors.getSelectedIndex();
+        defListModelHarbors.clear();
+        for (int i = 0; i < harborCollection.keys().size(); i++){
+            defListModelHarbors.addElement(harborCollection.keys().get(i));
+        }
+        if (defListModelHarbors.size() > 0 && (index == -1 || index >= defListModelHarbors.size())){
+            jListHarbors.setSelectedIndex(0);
+        }
+        else{
+            if (defListModelHarbors.size() > 0 && index > -1 && index < defListModelHarbors.size()){
+                jListHarbors.setSelectedIndex(index);
+            }
+        }
+    }
 
     public WindowControlHarbor(){
-        harbor=new Harbor<ITransport, LightMotor>(1100, 664);
+        harborCollection=new HarborCollection(1100, 664);
         canvasBoat.setModal(true);
         setBackground(Color.white);
         setLayout(null);
-        addButton(labelTake, 1040, 170, 189, 17);
-        addButton(labelPlace, 1050, 200, 57, 18);
-        addButton(btnParkingSkiff, 1041, 12, 149, 62);
-        addButton(btnParkingBoat, 1041, 95, 149, 62);
-        addButton(btnTake, 1070, 240, 95, 31);
+        addButton(labelHarbors, 1070, 9, 59, 17);
+        addButton(txtHarborName, 1018, 35, 172, 22);
+        addButton(btnAddHarbor, 1018, 63, 172, 32);
+        jListHarbors=new JList<String>(defListModelHarbors);
+        jListHarbors.setPrototypeCellValue("Установленный размер");
+        jListHarbors.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+        jListHarbors.addListSelectionListener(new listBoxChangeListener());
+        addButton(new JScrollPane(jListHarbors), 1018, 101, 172, 116);
+        addButton(btnRemoveHarbor, 1018, 223, 172, 32);
+        addButton(labelTake, 1020, 427, 189, 17);
+        addButton(labelPlace, 1030, 457, 57, 18);
+        addButton(btnParkingSkiff, 1021, 272, 149, 62);
+        addButton(btnParkingBoat, 1021, 349, 149, 62);
+        addButton(btnTake, 1050, 507, 95, 31);
+        addButton(btnShowBoat, 1050, 560, 95, 31);
         NumberFormat format = NumberFormat.getInstance();
         NumberFormatter formatter = new NumberFormatter(format);
         formatter.setAllowsInvalid(false);
         txtIndexPlace = new JFormattedTextField(formatter);
-        addButton(txtIndexPlace, 1110, 200, 42, 22);
+        addButton(txtIndexPlace, 1110, 457, 42, 22);
 
         btnParkingSkiff.addActionListener(new ActionListener() {
             @Override
@@ -53,7 +97,7 @@ public class WindowControlHarbor extends JPanel{
                 Color mainColor = JColorChooser.showDialog( WindowControlHarbor.this, "Выберите цвет лодки", Color.BLUE );
                 if (mainColor != null) {
                      boat=new Skiff(100, 1000, mainColor);
-                    if (harbor.addSkiff(boat) >-1) {
+                    if (harborCollection.getHarbor(jListHarbors.getSelectedValue()).addSkiff(boat) >-1) {
                         repaint();
                     } else {
                         JOptionPane.showMessageDialog(WindowControlHarbor.this, "Гавань переполнена", "Сообщение", JOptionPane.INFORMATION_MESSAGE );
@@ -70,7 +114,7 @@ public class WindowControlHarbor extends JPanel{
                     if(dopColor!=null){
                         boat = new Boat(100, 1000, mainColor, dopColor,
                                 true, true);
-                        if ((harbor.addSkiff(boat)>-1))
+                        if ((harborCollection.getHarbor(jListHarbors.getSelectedValue()).addSkiff(boat)>-1))
                         {
                             repaint();
                         }
@@ -87,18 +131,48 @@ public class WindowControlHarbor extends JPanel{
             public void actionPerformed(ActionEvent e) {
                 if (!Objects.equals(txtIndexPlace.getText(),""))
                 {
-                    boat = harbor.remove(Integer.parseInt(txtIndexPlace.getText()));
+                    boat = harborCollection.getHarbor(jListHarbors.getSelectedValue()).remove(Integer.parseInt(txtIndexPlace.getText()));
                     if (boat != null)
                     {
-                        canvasBoat.SetSkiff(boat);
-                        canvasBoat.setVisible(true);
+                        linkedListHarborRemoveBoat.add(boat);
 
                     }
-                    else
-                        JOptionPane.showMessageDialog(WindowControlHarbor.this , "Парковочное место пусто", "Сообщение", JOptionPane.INFORMATION_MESSAGE );
                     repaint();
                 }
             }
         });
+        btnShowBoat.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if(linkedListHarborRemoveBoat.size()>0){
+                    canvasBoat.SetSkiff(linkedListHarborRemoveBoat.pop());
+                    canvasBoat.setVisible(true);
+                }
+            }
+        });
+        btnAddHarbor.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if(txtHarborName.getText().equals("")){
+                    JOptionPane.showMessageDialog(null, "Введите название гавани", "Ошибка", JOptionPane.ERROR_MESSAGE);
+                }
+                else{
+                    harborCollection.AddHarbor(txtHarborName.getText());
+                    ReloadHarbors();
+                }
+            }
+        });
+        btnRemoveHarbor.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if(jListHarbors.getSelectedValue()!=null){
+                    if(JOptionPane.showConfirmDialog(null, "Удалить гавань " + jListHarbors.getSelectedValue() + "?") == 0){
+                        harborCollection.DelHarbor(jListHarbors.getSelectedValue());
+                        ReloadHarbors();
+                    }
+                }
+            }
+        });
+
     }
 }
